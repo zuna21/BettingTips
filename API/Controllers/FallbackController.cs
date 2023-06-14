0ec63240cbbd7217d3169392046ce43fb1621200
zuna21 <adir.zunic@gmail.com>
2023-06-14 20:18:12 +0200

    Return old fallback controller

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -1,19 +1,11 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
-using System.IO;
 
 public class FallbackController : Controller
 {
-    private readonly IWebHostEnvironment _environment;
-
-    public FallbackController(IWebHostEnvironment environment)
-    {
-        _environment = environment;
-    }
 
     public ActionResult Index()
     {
-        string webRootPath = _environment.WebRootPath;
+        string webRootPath = "/var/www/bettingmasteradvice/wwwroot";
         string filePath = Path.Combine(webRootPath, "index.html");
 
         return PhysicalFile(filePath, "text/HTML");
