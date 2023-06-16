d1888ffd1b4d32dc79a515801130c6b8d2577e65
zuna21 <adir.zunic@gmail.com>
2023-06-16 10:08:26 +0200

    Allowed all origin, it's not good- fix it

--- API/Program.cs
+++ API/Program.cs
@@ -47,8 +47,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(x => x.AllowAnyHeader()
     .AllowAnyMethod()
-    .AllowCredentials()
-    .WithOrigins("http://localhost:4200"));
+    .AllowAnyOrigin());
 
 app.UseStaticFiles();
 
