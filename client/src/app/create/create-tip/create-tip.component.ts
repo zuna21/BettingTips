045cdb304f50ce01802922eb003c316eee033e1c
zuna21 <adir.zunic@gmail.com>
2023-06-20 09:23:45 +0200

    Change tip create design

--- client/src/app/create/create-tip/create-tip.component.ts
+++ client/src/app/create/create-tip/create-tip.component.ts
@@ -12,7 +12,7 @@ const URL = 'htadkfjas;df';
 @Component({
   selector: 'app-create-tip',
   templateUrl: './create-tip.component.html',
-  styleUrls: ['./create-tip.component.css']
+  styleUrls: ['./create-tip.component.css'],
 })
 export class CreateTipComponent implements OnInit {
   private tips = new BehaviorSubject<Tip[]>([]);
@@ -23,18 +23,15 @@ export class CreateTipComponent implements OnInit {
     homeTeam: '',
     awayTeam: '',
     packages: [],
-    photo: null
+    photo: null,
   };
   selectedFile: File | null = null;
-
 
   constructor(
     private tipService: TipService,
     public packageService: PackageService,
     private fb: FormBuilder
   ) {}
-
-
 
   ngOnInit(): void {
     this.loadTips();
@@ -46,12 +43,12 @@ export class CreateTipComponent implements OnInit {
     this.createTipForm = this.fb.group({
       homeTeam: ['', Validators.required],
       awayTeam: ['', Validators.required],
-      photo: [null]
+      photo: [null],
     });
   }
 
   private setTips(newTips: Tip[]) {
-    this.tips.next(newTips)
+    this.tips.next(newTips);
   }
 
   private getTips() {
@@ -59,28 +56,32 @@ export class CreateTipComponent implements OnInit {
   }
 
   loadTips() {
-    this.tipService.getAllActiveTips().pipe(take(1))
+    this.tipService
+      .getAllActiveTips()
+      .pipe(take(1))
       .subscribe({
-        next: fetchedTips => this.setTips(fetchedTips)
+        next: (fetchedTips) => this.setTips(fetchedTips),
       });
   }
 
   loadPackages() {
-    this.packageService.getAllPackages().pipe(take(1))
+    this.packageService
+      .getAllPackages()
+      .pipe(take(1))
       .subscribe({
-        next: allPackages => this.packageService.setAllPackages(allPackages)
+        next: (allPackages) => this.packageService.setAllPackages(allPackages),
       });
   }
 
   onCheckPackage(e, selectedPackage: Package) {
     const isChecked: boolean = e.target.checked;
-    if (isChecked) 
-      this.selectedPackages.push(selectedPackage);
-    else 
+    if (isChecked) this.selectedPackages.push(selectedPackage);
+    else
       this.selectedPackages.splice(
-        this.selectedPackages.indexOf(selectedPackage), 1
+        this.selectedPackages.indexOf(selectedPackage),
+        1
       );
-    
+
     this.tipToCreate.packages = [...this.selectedPackages];
   }
 
@@ -90,51 +91,61 @@ export class CreateTipComponent implements OnInit {
     if (!this.tipToCreate.photo) return;
     this.tipToCreate.homeTeam = this.createTipForm.get('homeTeam').value;
     this.tipToCreate.awayTeam = this.createTipForm.get('awayTeam').value;
-    this.tipService.createTip(this.tipToCreate).pipe(take(1))
+    this.tipService
+      .createTip(this.tipToCreate)
+      .pipe(take(1))
       .subscribe({
-        next: newTip => {
+        next: (newTip) => {
           const currentTips: Tip[] = this.getTips();
           const updatedTips: Tip[] = [...currentTips, newTip];
           this.setTips(updatedTips);
           this.createTipForm.reset();
-        }
+          this.tipToCreate.photo = null;
+          this.tipToCreate.awayTeam = '';
+          this.tipToCreate.homeTeam = '';
+        },
       });
   }
 
   deleteAllTips() {
     const currentTips = this.getTips();
     if (currentTips.length === 0) return;
-    this.tipService.deleteAllTips().pipe(take(1))
+    this.tipService
+      .deleteAllTips()
+      .pipe(take(1))
       .subscribe({
-        next: _ => {
+        next: (_) => {
           this.tips.next([]);
-        }
-      })
+        },
+      });
   }
 
+  selectFileToUpload(event) {
+    this.selectedFile = event.target.files[0];
+  }
 
- selectFileToUpload(event) {  
-     this.selectedFile = event.target.files[0];
- }  
-
- uploadFile() {
+  uploadFile() {
     if (!this.selectedFile) return;
     const formData = new FormData();
     formData.append('photo', this.selectedFile);
-    this.tipService.createPhoto(formData)
+    this.tipService
+      .createPhoto(formData)
       .pipe(take(1))
-      .subscribe({next: response => this.tipToCreate.photo = response});
- }
+      .subscribe({ next: (response) => (this.tipToCreate.photo = response) });
+  }
 
- onDeleteTip(tipToDelete: Tip) {
-  this.tipService.deleteTip(tipToDelete).pipe(take(1))
-    .subscribe({
-      next: _ => {
-        const currentTips = this.getTips();
-        const updatedTips = currentTips.filter(x => x.id !== tipToDelete.id);
-        this.tips.next(updatedTips);
-      }
-    });
- }
-
+  onDeleteTip(tipToDelete: Tip) {
+    this.tipService
+      .deleteTip(tipToDelete)
+      .pipe(take(1))
+      .subscribe({
+        next: (_) => {
+          const currentTips = this.getTips();
+          const updatedTips = currentTips.filter(
+            (x) => x.id !== tipToDelete.id
+          );
+          this.tips.next(updatedTips);
+        },
+      });
+  }
 }
