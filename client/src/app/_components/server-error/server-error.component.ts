5c6502b02dd0c4c7418f05c5924f938a7a89a4ae
zuna21 <adir.zunic@gmail.com>
2023-06-28 11:02:13 +0200

    fixed server error

--- client/src/app/_components/server-error/server-error.component.ts
+++ client/src/app/_components/server-error/server-error.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./server-error.component.css']
 })
 export class ServerErrorComponent {
+  private router: Router = inject(Router);
 
+  onLoginPage() {
+    if (!localStorage.getItem('userToken')) return;
+    localStorage.removeItem('userToken');
+    this.router.navigateByUrl('/login');
+  }
 }
--- client/src/app/_guards/auth.guard.ts
+++ client/src/app/_guards/auth.guard.ts
@@ -1,18 +1,8 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { CanActivateFn } from '@angular/router';
 
 export function authGuard(): CanActivateFn {
   return () => {
-    /* if (!localStorage.getItem('userToken')) return false;
-        return true; */
-    const router: Router = inject(Router);
-    const token = localStorage.getItem('userToken');
-
-    if (token) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+    if (!localStorage.getItem('userToken')) return false;
+        return true;
   };
 }
